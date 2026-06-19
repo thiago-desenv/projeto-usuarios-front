d5fcf85b8879c14f51499ad3c9b658dc8af27d75
thiago_osim <thiago_osim@hotmail.com>
2026-06-18 21:54:54 -0300

    Criado os componentes de login e user-infos

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  private readonly _router = inject(Router);
+
+  onLogin() {
+
+  }
+}
---
+++ src/app/components/user-infos/user-infos.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-infos',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-infos.component.html',
+  styleUrl: './user-infos.component.scss'
+})
+export class UserInfosComponent {
+  userInfosForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+}
