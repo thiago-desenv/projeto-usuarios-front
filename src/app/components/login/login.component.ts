f5ba5fa18f06d79520ecceea17f213e5c56e0d3e
thiago_osim <thiago_osim@hotmail.com>
2026-07-14 22:19:10 -0300

    Realizado a chamado do método login no loginComponent

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -16,8 +17,13 @@ export class LoginComponent {
   });
 
   private readonly _router = inject(Router);
-
+  private readonly _loginService = inject(LoginService);
   onLogin() {
-
+    this._loginService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe({
+        next: () => {
+          this._router.navigate(['user-infos']);
+        },
+        error: () => {  }
+    });
   }
 }
