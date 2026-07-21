acf181bf2810333f0aec5e12d2b8e67d1b552dfd
thiago_osim <thiago_osim@hotmail.com>
2026-07-20 22:48:20 -0300

    Adicionado lógica para armazenar o token no localStorage

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,11 @@ export class LoginService {
   private readonly _httpClient = inject(HttpClient);
 
   login(username: string, password: string): Observable<{ token: string }> {
-    return this._httpClient.post<{ token: string }>('http://localhost:3000/login', { username, password });
+    return this._httpClient.post<{ token: string }>('http://localhost:3000/login', { username, password }).pipe(
+      map((tokenResponse) => {
+        localStorage.setItem('token', tokenResponse.token);
+        return tokenResponse;
+      })
+    );
   }
 }
