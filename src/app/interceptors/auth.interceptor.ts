b514841ccada417c0ef6c1d3a9ff32c658634577
thiago_osim <thiago_osim@hotmail.com>
2026-09-17 23:05:45 -0300

    Criado a classe authInterceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,3 +1,9 @@
-export function authInterceptor() {
+import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
 
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  console.log('authInterceptor');
+  console.log('req', req);
+
+  return next(req);
 }
